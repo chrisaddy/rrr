598eff1434ad589776ac3e3917c28bfdca00f9c6
Chris Addy <chris.william.addy@gmail.com>
2016-10-14 03:15:57 -0400

    Create weighted_mat.R

---
+++ R/weighted_mat.R
@@ -0,0 +1,10 @@
+weighted_mat <- function(var_x, var_y, gamma_mat){ 
+			sig_XX <- cov_mat(var_x, var_x)
+			sig_XY <- cov_mat(var_x, var_y)
+			sig_YX <- cov_mat(var_y, var_x) 
+			sqrt_matrix(gamma_mat) %*%
+				sig_YX %*%
+				solve(sig_XX) %*%
+				sig_XY %*%
+				sqrt_matrix(gamma_mat)
+}
