a26463850afb3626b62a46c15311e48cbc00aee2
Chris Addy <chris.william.addy@gmail.com>
2016-10-14 03:15:36 -0400

    Move weighted_mat()

--- utils.R
+++ utils.R
@@ -6,17 +6,6 @@ require(ggplot2)
 require(cvTools)
 require(matrixcalc)
 
-weighted_mat <- function(var_x, var_y, gamma_mat){ 
-			sig_XX <- cov_mat(var_x, var_x)
-			sig_XY <- cov_mat(var_x, var_y)
-			sig_YX <- cov_mat(var_y, var_x) 
-			sqrt_matrix(gamma_mat) %*%
-				sig_YX %*%
-				solve(sig_XX) %*%
-				sig_XY %*%
-				sqrt_matrix(gamma_mat)
-}
-
 V_t <- function(var_x, var_y, gamma_mat, rank){
        		eigen(weighted_mat(var_x,
        						   var_y,
