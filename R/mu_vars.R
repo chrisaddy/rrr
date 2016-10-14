0047e74c27df47f9836e2d537725677aead620a6
Chris Addy <chris.william.addy@gmail.com>
2016-10-14 17:07:03 -0400

    Add documentation for mu_vars.R

--- R/mu_vars.R
+++ R/mu_vars.R
@@ -1,3 +1,7 @@
+#' Mean Matrix
+#'
+# \code{mu_vars} creates a matrix of sample means from a matrix \eqn{X} with the same dimensions as \eqn{X}. 
+
 mu_vars <- function(var_matrix){
 				replicate(dim(var_matrix)[2],
 				rowMeans(var_matrix))
