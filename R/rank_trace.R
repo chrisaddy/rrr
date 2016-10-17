834cb162dfe681c20242957ab159c4f4c02542ba
Chris Addy <chris.william.addy@gmail.com>
2016-10-17 16:32:35 -0400

    Move sigma_ee_t

--- R/rank_trace.R
+++ R/rank_trace.R
@@ -1,6 +1,11 @@
+sigma_ee_t <- function(var_x, var_y, gamma_mat, rank){
+		e <- var_y - C_t(var_x, var_y, gamma_mat, rank) %*% var_x
+		e %*% t(e) / (dim(var_x)[2] - dim(var_x)[1])
+}
+
 sigma_ee_full <- function(var_x, var_y, gamma_mat){
        			s <- dim(var_y)[1]
-			sigma_ee_t(var_x, var_y, gamma_mat, s)
+				sigma_ee_t(var_x, var_y, gamma_mat, s)
 }
 
 delta_C <- function(x, y, gamma_mat, rank){
