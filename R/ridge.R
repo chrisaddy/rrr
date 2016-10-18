854601a4d037b9d615311197fcd231a64993394e
Chris Addy <chris.william.addy@gmail.com>
2016-10-18 13:26:11 -0400

    Update documentation

--- R/ridge.R
+++ R/ridge.R
@@ -1,11 +1,11 @@
-#` Ridge Regression
-#`
-#` \code{ridge} fits a generalized ridge regression model by minimizing sum of squared error subject to an elliptical restriction on model parameters.
-#`
-#` @param x data frame of input variables
-#` @param y vector of response variables
-#` @param tuner small constant to augment the diagonal of 
-#` @radius radius of 
+#' Ridge Regression
+#'
+#' \code{ridge} fits a generalized ridge regression model by minimizing sum of squared error subject to an elliptical restriction on model parameters.
+#'
+#' @param x data frame of input variables
+#' @param y vector of response variables
+#' @param k small constant to augment the diagonal of the covariance matrix \eqn{\Sigma_{XX}}
+#' @radius radius of 
 
 ridge <- function(x, y, k = 0){
 			x_mat <- as.matrix(x)
