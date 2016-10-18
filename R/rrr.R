9dc1f47e909311316536d6984512f8c37412c4fb
Chris Addy <chris.william.addy@gmail.com>
2016-10-18 01:56:45 -0400

    Combine rrr into single function

--- R/rrr.R
+++ R/rrr.R
@@ -1,44 +1,3 @@
-V_t <- function(x, y, gamma_mat, rank){
-				if(rank == "full"){
-					reduce_rank <- min(dim(x)[2], dim(y)[2])
-				} else {
-					reduce_rank <- rank
-				}
-				w <- weighted_mat(x, y, gamma_mat)
-				eigen(w)$vectors[,1:rank] %>%
-				as.matrix(ncol = rank)
-}
-
-A_t <- function(x, y, gamma_mat, rank = "full"){
-		if(rank == "full"){
-			reduce_rank <- min(dim(x)[2], dim(y)[2])
-		} else {
-			reduce_rank <- rank
-		}
-		solve(sqrt_matrix(gamma_mat)) %*% 
-			V_t(x, y, gamma_mat, reduce_rank)
-}
-
-B_t <- function(x, y, gamma_mat, rank = "full"){
-		if(rank == "full"){
-			reduce_rank <- min(dim(x)[2], dim(y)[2])
-		} else {
-			reduce_rank <- rank
-		}
-		t(V_t(organize(x), organize(y), gamma_mat, reduce_rank)) %*% sqrt_matrix(gamma_mat) %*% cov_mat(organize(y), organize(x)) %*% solve(cov_mat(organize(x), organize(x)))
-}
-
-C_t <- function(x, y, gamma_mat, rank) {
-		if(rank == "full"){
-			reduce_rank <- min(dim(x)[2], dim(y)[2])
-		} else {
-			reduce_rank <- rank
-		}
-		A_t(x, y, gamma_mat, reduce_rank) %*% 
-			B_t(x, y, gamma_mat, reduce_rank)
-}
-
-
 #' Reduced-Rank Regression
 #'
 #' \code{rrr} fits a reduced-rank regression model.
@@ -58,16 +17,17 @@ rrr <- function(x, y, gamma_matrix, rank = "full"){
 		}
 		x_organize <- organize(x)
 		y_organize <- organize(y)
-		weighted_matrix <- sqrt_matrix(gamma_matrix)
-
-}
-
-weighted_mat <- function(x, y, gamma_mat){
-			x_organize <- organize(x)
-			y_organize <- organize(y)
-			sqrt_matrix(gamma_mat) %*%
-				cov_mat(y_organize, x_organize) %*%
-				solve(cov_mat(x_organize, x_organize)) %*%
-				cov_mat(x_organize, y_organize) %*%
-				sqrt_matrix(gamma_mat)
+		weighted_matrix <- sqrt_matrix(gamma_matrix) %>%
+								cov_matrix(y_organize, x_organize) %*%
+								solve(cov_matrix(x_organize, x_organize)) %*%
+								cov_matrix(x_organize, y_organize) %*%
+								sqrt_matrix(gamma_mat)
+		V_t <- eigen(weighted_matrix)[,1:rank] %>% as.matrix(ncol = rank)
+		A_t <- solve(sqrt_matrix(gamma_mat)) %*% 
+								V_t(x_organize, y_organize, gamma_mat, reduce_rank)
+		B_t <- t(V_t(x_organize, organize(y), gamma_mat, reduce_rank)) %*% 
+								sqrt_matrix(gamma_mat) %*% 
+								cov_matrix(organize(y), organize(x)) %*% 
+								solve(cov_matrix(organize(x), organize(x)))
+		A_t
 }
