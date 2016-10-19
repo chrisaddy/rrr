f879ac91514ef044ca47d451d0b3862f39ba5e4c
Chris Addy <chris.william.addy@gmail.com>
2016-10-18 23:19:38 -0400

    Move rank_error_rate to workbench

--- R/rank_error_rate.R
+++
@@ -1,8 +0,0 @@
-rank_error_rate <- function(x, y, gamma_mat, rank, folds){
-			k <- folds$K
-			errors <- c()
-			for(i in 1:k){
-				errors[i] <- error(x, y, gamma_mat, rank, folds, i)
-			}
-			mean(errors) / (dim(y)[2])
-}
