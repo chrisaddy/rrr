647f21740ad4c0d159e73d6b00ade107471dc1ef
Chris Addy <chris.william.addy@gmail.com>
2016-11-10 14:55:01 -0500

    Add cva residuals

---
+++ R/cva_diagnostics.R
@@ -0,0 +1,11 @@
+#' Residuals of Reduced-Rank Canonical Variate
+#'
+#' \code{cva_residuals} returns the multivariate residuals of the reduced-rank CVA regression
+#'
+#' @inheritParams rrcva
+#'
+#' @export
+
+cva_residuals <- function(x, y, rank = "full", type = "cov"){
+
+}
