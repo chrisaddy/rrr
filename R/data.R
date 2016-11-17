9f86221517757469c163518d345490e14ddd6e1c
chrisaddy <chris.william.addy@gmail.com>
2016-11-17 10:53:39 -0500

    Add COMBO17 data

--- R/data.R
+++ R/data.R
@@ -8,3 +8,12 @@
 #' @references A. Izenman (2008) \emph{Modern Multivariate Statistical Techniques}. Springer.
 "pendigits"
 
+#' MMST COMBO17 DATA
+#' 
+#' COMBO-17 galaxy photometric catalogue, 216, 219, 235
+#'
+#' @format A data frame with 3462 observations on 65 numeric variables.
+#' 
+#' @references A. Izenman (2008). \emph{Modern Multivariate Statistical Techniques}. Springer.
+#' @references Wolf, C. Meisenheimer, M., Kleinheinrich, M., Borch, A., Dye, S., Gray, M., Wisotski, L., Bell, E.F., Rix, H., W. Cimatti, A., Hasinger, G., and Szokoly, G. (2004). \emph{A catalogue of the Chandra Deep Field South with multi-colour classification and photometric redshifts from COMBO-17, Astronomy & Astrophysics}. \ur{arXiv:astro-ph/0403666v1}
+"COMBO17"
