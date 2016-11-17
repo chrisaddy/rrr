9737c8ddefbb8cbb56dc447fe98aeedabdeca278
chrisaddy <chris.william.addy@gmail.com>
2016-11-16 23:37:57 -0500

    Update reference page

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' MMST PENDIGITS DATA
+#'
+#' pen-based handwritten digit recognition, 211, 234, 274, 348, 391, 631
+#'
+#' @format a data frame with 10992 observations on 36 unnamed variables
+#'
+#' @source \url{http://archive.ics.uci.edu/ml/datasets.html}
+#' @references A. Izenman (2008) \emph{Modern Multivariate Statistical Techniques}. Springer.
+"pendigits"
+
